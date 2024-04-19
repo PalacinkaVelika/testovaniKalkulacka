cddb7e5750de3ed4a7df89d8e927216a380da6c3
Pepa Bůh <128801641+PalacinkaVelika@users.noreply.github.com>
2024-04-19 17:18:20 +0200

    buttons added

--- kalkulacka.py
+++ kalkulacka.py
@@ -7,3 +7,16 @@ class Kalkulacka():
     # vrátí výsledek horniho radku
     def vypocitej(self):
         return eval(self._horni_radek)
+    
+    def tlacitko_plus(self):
+        pass
+    def tlacitko_minus(self):
+        pass
+    def tlacitko_deleno(self):
+        pass
+    def tlacitko_krat(self):
+        pass
+    def tlacitko_rovna_se(self):
+        pass
+    def tlacitko_cislo(self, cislo):
+        pass
